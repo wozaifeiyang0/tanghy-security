28019d789b664e4da4b000329380b39563f2c71f
Tang huai yu <tanghy@jianjukeji.com>
2020-12-03 15:39:30 +0800

    更新凯撒加密方法： 去掉方法异常返回

--- src/main/java/space/tanghy/security/util/CaesarUtils.java
+++ src/main/java/space/tanghy/security/util/CaesarUtils.java
@@ -2,73 +2,78 @@ package space.tanghy.security.util;
 
 
 /**
- *  author tanghy
- *  time 2020-12-03
- *  <p>凯撒加密，解密工具，内置明文字典表和密文字典表</p>
+ * author tanghy
+ * time 2020-12-03
+ * <p>凯撒加密，解密工具，内置明文字典表和密文字典表</p>
  */
 public class CaesarUtils {
 
 
     // 明文字典表
-    private final static char[] caesarDictionary = new char[]{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
-            'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
-            '1','2','3','4','5','6','7','8','9','0'
+    private final static char[] caesarDictionary = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'
     };
     // 密文字典表
-    private final static char[] caesarComparison = new char[]{'h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g',
-            '1','2','3','4','5','6','7','8','9','0',
-            'H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G'
+    private final static char[] caesarComparison = new char[]{'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+            '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+            'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G'
     };
 
 
     /**
      * 加密方法
-     * @param mw 需要加密的字符串
+     *
+     * @param mw     需要加密的字符串
      * @param offset 偏移量
      * @return 返回加密的字符串
      */
-    public static String encrypt(String mw, int offset) throws Exception {
-        return handler(mw, offset,caesarDictionary, caesarComparison, false);
+    public static String encrypt(String mw, int offset) {
+        return handler(mw, offset, caesarDictionary, caesarComparison, false);
     }
 
     /**
      * 解密方法
-     * @param mw 加密后的字符串
+     *
+     * @param mw     加密后的字符串
      * @param offset 偏移量
      * @return 返回解密后的字符串
      */
-    public static String decrypt(String mw, int offset) throws Exception {
-        return handler(mw,offset,caesarComparison,caesarDictionary, true);
+    public static String decrypt(String mw, int offset) {
+        return handler(mw, offset, caesarComparison, caesarDictionary, true);
     }
 
     /**
      * 加密方法
+     *
      * @param mw 需要加密的字符串
      * @return 返回加密的字符串
      */
-    public static String encrypt(String mw) throws Exception {
-        return handler(mw, 0,caesarDictionary, caesarComparison, false);
+    public static String encrypt(String mw) {
+        return handler(mw, 0, caesarDictionary, caesarComparison, false);
     }
 
     /**
      * 解密方法
+     *
      * @param mw 加密后的字符串
      * @return 返回解密后的字符串
      */
-    public static String decrypt(String mw) throws Exception {
-        return handler(mw,0,caesarComparison,caesarDictionary, true);
+    public static String decrypt(String mw) {
+        return handler(mw, 0, caesarComparison, caesarDictionary, true);
     }
 
     /**
-     *  通用处理方法
-     * @param mw 加密或解密的字符串
-     * @param offset 偏移量，数值要大于0
+     * 通用处理方法
+     *
+     * @param mw               加密或解密的字符串
+     * @param offset           偏移量，数值要大于0
      * @param caesarDictionary 明文字符串对应表
      * @param caesarComparison 密文字符串对应表
-     * @param isDecrypt 为true时加密，false时解密
+     * @param isDecrypt        为true时加密，false时解密
      * @return 返回加密或者解密后的字符串
      */
-    private static String handler(String mw, int offset, char[] caesarDictionary, char[] caesarComparison, boolean isDecrypt) throws Exception {
+    private static String handler(String mw, int offset, char[] caesarDictionary, char[] caesarComparison, boolean isDecrypt) {
 
 
         StringBuilder result = new StringBuilder();
@@ -82,7 +87,7 @@ public class CaesarUtils {
                     if (!isDecrypt) {
                         if (offsetIndex + i >= caesarComparison.length) {
                             jm.append(caesarDictionary[offsetIndex + i - caesarComparison.length]);
-                        } else if (offsetIndex + i < 0){
+                        } else if (offsetIndex + i < 0) {
                             jm.append(caesarDictionary[i + offsetIndex + caesarComparison.length]);
                         } else {
                             jm.append(caesarDictionary[i + offsetIndex]);
@@ -91,7 +96,7 @@ public class CaesarUtils {
 
                         if (i - offsetIndex < 0) {
                             jm.append(caesarDictionary[i - offsetIndex + caesarComparison.length]);
-                        } else if (i - offsetIndex >= caesarComparison.length){
+                        } else if (i - offsetIndex >= caesarComparison.length) {
                             jm.append(caesarDictionary[i - offsetIndex - caesarComparison.length]);
                         } else {
                             jm.append(caesarDictionary[i - offsetIndex]);
